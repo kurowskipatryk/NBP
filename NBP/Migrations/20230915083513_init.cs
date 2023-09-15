0cb8d7c9351acc1626605876210dd5533c6cef68
kurowskipatryk <kurowski.patryk24@gmail.com>
2023-09-15 10:37:48 +0200

    migrations enable

---
+++ NBP/Migrations/20230915083513_init.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NBP.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tables",
+                columns: table => new
+                {
+                    Id_Table = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Table = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    No = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EffectiveDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tables", x => x.Id_Table);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rates",
+                columns: table => new
+                {
+                    Id_Rate = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Id_Table = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Mid = table.Column<double>(type: "float", nullable: false),
+                    Bid = table.Column<double>(type: "float", nullable: false),
+                    Ask = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rates", x => x.Id_Rate);
+                    table.ForeignKey(
+                        name: "FK_Rates_Tables_Id_Table",
+                        column: x => x.Id_Table,
+                        principalTable: "Tables",
+                        principalColumn: "Id_Table",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rates_Id_Table",
+                table: "Rates",
+                column: "Id_Table");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rates");
+
+            migrationBuilder.DropTable(
+                name: "Tables");
+        }
+    }
+}
